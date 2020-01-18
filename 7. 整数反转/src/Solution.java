65bf88a7753493c1265b226d7704daf0716a8c04
JingqingLin <JingqingLin@live.com>
2020-01-19 01:22:08 +0800

    第六、七题及一些修改

---
+++ 7. 整数反转/src/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int temp = x % 10;
+            x /= 10;
+            if (result > 214748364 || result < -214748364) {
+                return 0;
+            }
+            result = 10 * result + temp;
+        }
+        return result;
+    }
+    public static void main(String []args) {
+        System.out.println(reverse(2147483642
+        ));
+    }
+}
