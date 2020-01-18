6f24665712df0154d462ed5d51c20ea136922acc
JingqingLin <JingqingLin@live.com>
2020-01-18 12:39:44 +0800

    第四题

---
+++ 4. 寻找两个有序数组的中位数/src/Solution.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int j1 = nums1.length,j2 = nums2.length;
+        int k = (j1 + j2)/2;
+        if (j1 == 0) {
+            return (double) (nums2[(j2 - 1)/2]+nums2[j2/2])/2;
+        }
+        else if (j2 == 0) {
+            return (double) (nums1[(j1 - 1)/2]+nums1[j1/2])/2;
+        }
+        else {
+            int left = (j1 + j2 + 1) / 2;
+            int right = (j1 + j2 + 2) / 2;
+            // left/right 代表奇偶的情况
+            return (getKth(nums1, 0, j1 - 1, nums2, 0, j2 - 1, left) + getKth(nums1, 0, j1 - 1, nums2, 0, j2 - 1, right)) * 0.5;
+        }
+    }
+    private static int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        // 始终让短的数组长度为 len1
+        if (len1 > len2) {
+            return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        }
+        // len1 == 0 或 k == 1 时递归结束
+        if (len1 == 0) {
+            return nums2[start2 + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        }
+        else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        }
+    }
+    public static void main(String []args) {
+        System.out.println(findMedianSortedArrays(new int[]{2,3,4,5},new int[]{}));
+    }
+}
