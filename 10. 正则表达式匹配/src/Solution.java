68e2cdf7b6b69bddded46e7c0356426656bf2b8b
JingqingLin <JingqingLin@live.com>
2020-01-19 17:37:10 +0800

    第10、11题及一些修改

--- 10. 正则表达式匹配/src/Solution.java
+++ 10. 正则表达式匹配/src/Solution.java
@@ -1,8 +1,27 @@
+// 官方题解：回溯
+
 public class Solution {
     public static boolean isMatch(String s, String p) {
-        return true;
+        if (p.isEmpty()) {
+            return s.isEmpty();
+        }
+
+        // p 的匹配模式有两种 1. 单字符 a/b/c/.../. 2. 双字符 a*/b*/c*.../.*
+        boolean first_match = (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+
+        // p 下个字符为 * ，因此 p 中 a*/b*/c*.../.* 为当前匹配
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            // isMatch(s, p.substring(2))： s 当前字符与当前 p 不匹配或匹配完毕，尝试把 p 向后推 2 位
+            // isMatch(s.substring(1), p))：* 能匹配多个字符，把 s 向后推一位继续和 * 匹配
+            return (isMatch(s, p.substring(2)) ||
+                    (first_match && isMatch(s.substring(1), p)));
+        }
+        // 单个字符 a/b/c/.../. 为当前匹配
+        else {
+            return first_match && isMatch(s.substring(1), p.substring(1));
+        }
     }
     public static void main(String []args) {
-
+        System.out.println(isMatch("aabb", ".*b"));
     }
 }
---
+++ 11. 盛最多水的容器/src/Solution.java
@@ -0,0 +1,23 @@
+// 官方题解：双指针法（移动较短的那根）
+// 由于面积取决于边长短的那一端假设为m，所以要想得到比当前更大的面积，
+// 边长短的那一端必须舍弃，因为如果不舍弃，高最大就是m，而随着指针的移动宽会一直减小，因此面积只会越来越小。
+
+public class Solution {
+    public static int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int maxArea = 0;
+        while (i < j) {
+            maxArea = Math.max((height[i]<height[j]) ? (j-i)*height[i] : (j-i)*height[j], maxArea);
+            if (height[i] < height[j]) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+    public static void main(String []args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
