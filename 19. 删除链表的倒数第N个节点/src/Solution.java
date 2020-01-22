5eee501d72f311f575f1a8092d85ef0d231f5761
JingqingLin <JingqingLin@live.com>
2020-01-22 22:09:15 +0800

    第19题及一些修改

--- 19. 删除链表的倒数第N个节点/src/Solution.java
+++ 19. 删除链表的倒数第N个节点/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Solution {
     public static class ListNode {
        int val;
@@ -5,9 +7,34 @@ public class Solution {
        ListNode(int x) { val = x; }
     }
     public static ListNode removeNthFromEnd(ListNode head, int n) {
+        HashMap<Integer, ListNode> nodeHashMap = new HashMap<>();
+        ListNode temp = head;
+        int i = 1;
+        while (temp != null) {
+            nodeHashMap.put(i, temp);
+            i++;
+            temp = temp.next;
+        }
+//        System.out.printf("长度为%d\n", i - 1);
+        // 若为第一个节点
+        if (n == i - 1) {
+            head = head.next;
+        }
+        else {
+            nodeHashMap.get(i - 1 - n).next = nodeHashMap.get(i + 1 - n);
+        }
         return head;
     }
     public static void main(String []args) {
-
+        ListNode l1 = new ListNode(1);
+//        l1.next = new ListNode(2);
+//        l1.next.next = new ListNode(3);
+//        l1.next.next.next = new ListNode(4);
+//        l1.next.next.next.next = new ListNode(5);
+        ListNode lSum = removeNthFromEnd(l1, 1);
+        while (lSum != null) {
+            System.out.println(lSum.val);
+            lSum = lSum.next;
+        }
     }
 }
--- 2. 两数相加/src/Solution.java
+++ 2. 两数相加/src/Solution.java
@@ -12,26 +12,28 @@ public class Solution {
         ListNode l4 = l3;
         int carry = 0;
         while (l1 != null || l2 != null) {
-            l3.val = l1.val +l2.val +carry;
+            assert l1 != null;
+            l3.val = l1.val +l2.val + carry;
             if (l3.val >= 10) {
                 carry = 1;
                 l3.val -= 10;
             }
-            else
+            else {
                 carry = 0;
+            }
             if (l1.next != null && l2.next != null) {
                 l3.next = new ListNode(0);
                 l1 = l1.next;
                 l2 = l2.next;
                 l3 = l3.next;
             }
-            else if (l1.next != null && l2.next == null) {
+            else if (l1.next != null) {
                 l3.next = new ListNode(0);
                 l1 = l1.next;
                 l2.val = 0;
                 l3 = l3.next;
             }
-            else if (l2.next != null && l1.next == null) {
+            else if (l2.next != null) {
                 l3.next = new ListNode(0);
                 l2 = l2.next;
                 l1.val = 0;
@@ -39,8 +41,9 @@ public class Solution {
             }
             else {
                 // 最高位进位
-                if (carry == 1)
+                if (carry == 1) {
                     l3.next = new ListNode(1);
+                }
                 break;
             }
         }
