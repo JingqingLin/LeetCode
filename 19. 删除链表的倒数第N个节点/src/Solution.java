21472880fb3a453ebbf0333099e3799648431d60
JingqingLin <JingqingLin@live.com>
2020-01-22 19:34:37 +0800

    第18题及一些修改

---
+++ 19. 删除链表的倒数第N个节点/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static class ListNode {
+       int val;
+       ListNode next;
+       ListNode(int x) { val = x; }
+    }
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        return head;
+    }
+    public static void main(String []args) {
+
+    }
+}
