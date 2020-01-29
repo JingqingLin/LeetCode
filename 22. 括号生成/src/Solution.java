4e0af60841a759b9036a7d4831685db4d59a336a
JingqingLin <JingqingLin@live.com>
2020-01-29 16:53:55 +0800

    22题

---
+++ 22. 括号生成/src/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Solution {
+    public static List<String> generateParenthesis(int n) {
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("()");
+        if (n == 1) {
+            List<String> result = new ArrayList<>(stringSet);
+            return result;
+        }
+        else {
+            List<String> result = new ArrayList<>(insertParenthesis(stringSet, n));
+            return result;
+        }
+    }
+    private static Set<String> insertParenthesis(Set<String> stringSet, int n) {
+        if (n == 1) {
+            return stringSet;
+        }
+        else {
+            Set<String> newStringSet = new HashSet<>();
+            // Set 中 String 的每个位置都插入 "()"
+            for (String str : stringSet) {
+                for (int i = str.length()-1; i >= 0; i--) {
+                    StringBuilder tempStr = new StringBuilder();
+                    tempStr.append(str);
+                    tempStr.insert(i, "()");
+                    newStringSet.add(String.valueOf(tempStr));
+                }
+            }
+            return insertParenthesis(newStringSet, n-1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(2));
+    }
+}
