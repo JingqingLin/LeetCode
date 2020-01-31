12a4e0c109c1125e69e87f265c28bb9fd17bd68a
JingqingLin <JingqingLin@live.com>
2020-01-31 21:45:54 +0800

    双指针题

---
+++ 算法思想/双指针/345. 反转字符串中的元音字母/src/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public static String reverseVowels(String s) {
+        char []result = s.toCharArray();
+        int l = 0, r = s.length()-1;
+        while (l < r) {
+            while (isVowel(result[l]) &&
+                    isVowel(result[r]) && l < r) {
+                result[l] = s.charAt(r);
+                result[r] = s.charAt(l);
+                l++;
+                r--;
+            }
+            while (!isVowel(result[l]) && l < r) {
+                l++;
+            }
+            while (!isVowel(result[r]) && l < r) {
+                r--;
+            }
+        }
+        return String.valueOf(result);
+    }
+    public static boolean isVowel(char c) {
+        return c == 65 || c == 69 || c == 73 || c == 79 || c == 85 || c == 97 || c == 101 || c == 105 || c == 111 || c == 117;
+    }
+    public static void main(String[] args) {
+        System.out.println(reverseVowels("leaeou"));
+    }
+}
