2e7987912080c4ba8c856b89a0d7da8926f63453
JingqingLin <JingqingLin@live.com>
2020-02-01 21:07:41 +0800

    双指针做完

---
+++ 算法思想/双指针/524. 通过删除字母匹配到字典里最长单词/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// 注意条件：字典序
+public class Solution {
+    public static String findLongestWord(String s, List<String> d) {
+        int maxLen = 0;
+        String result = "";
+        for (String dicStr : d) {
+            int p = 0, q = 0;
+            while (q < dicStr.length() && p < s.length() && maxLen <= dicStr.length()) {
+                if (dicStr.charAt(q) == s.charAt(p)) {
+                    p++;q++;
+                }
+                else {
+                    p++;
+                }
+            }
+            // 字典序比较
+            if (q == dicStr.length() && (maxLen < q || (maxLen == q && dicStr.compareTo(result) < 0))) {
+                maxLen = dicStr.length();
+                result = dicStr;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        List<String> d = new ArrayList<>();
+        d.add("ba");
+        d.add("ab");
+        d.add("a");
+        d.add("b");
+        System.out.println(findLongestWord("aba", d));
+    }
+}
