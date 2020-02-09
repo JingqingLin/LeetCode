910d7bf2b31fddb21b8f633a072bbf8e073afa14
JingqingLin <JingqingLin@live.com>
2020-02-10 00:55:03 +0800

    穷举二叉搜索树

--- 算法思想/分治/95. 不同的二叉搜索树 II/src/Solution.java
+++ 算法思想/分治/95. 不同的二叉搜索树 II/src/Solution.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -13,6 +13,36 @@ public class Solution {
 
     public static List<TreeNode> generateTrees(int n) {
         List<TreeNode> treeNodeList = new ArrayList<>();
+        if (n == 0) {
+            return treeNodeList;
+        }
+        treeNodeList = generateTrees(1, n);
+        return treeNodeList;
+    }
+
+    private static List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> treeNodeList = new ArrayList<>();
+        if (end - start + 1 == 0) {
+            TreeNode root = null;
+            treeNodeList.add(root);
+            return treeNodeList;
+        }
+        if (end - start + 1 == 1) {
+            treeNodeList.add(new TreeNode(start));
+            return treeNodeList;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTreeNodeList = generateTrees(start, i - 1);
+            List<TreeNode> rightTreeNodeList = generateTrees(i + 1, end);
+            for (TreeNode l : leftTreeNodeList) {
+                for (TreeNode r : rightTreeNodeList) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    treeNodeList.add(root);
+                }
+            }
+        }
         return treeNodeList;
     }
 
