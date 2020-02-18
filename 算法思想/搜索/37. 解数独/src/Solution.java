d262089f69cd08f51e8b5eb182485b6a23c22d5e
JingqingLin <JingqingLin@live.com>
2020-02-18 17:04:55 +0800

    搜索

---
+++ 算法思想/搜索/37. 解数独/src/Solution.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class Solution {
+    private static boolean[][] rowsUsed = new boolean[9][10];
+    private static boolean[][] colsUsed = new boolean[9][10];
+    private static boolean[][] cubesUsed = new boolean[9][10];
+    private static char[][] board;
+
+    public static void solveSudoku(char[][] board) {
+        Solution.board = board;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                int num = board[i][j] - '0';
+                rowsUsed[i][num] = true;
+                colsUsed[j][num] = true;
+                cubesUsed[cubeNum(i, j)][num] = true;
+            }
+        }
+        backtracking(0, 0);
+    }
+
+    private static boolean backtracking(int row, int col) {
+        // 已填好数字就跳过（包含换行处理）
+        while (row < 9 && board[row][col] != '.') {
+            row = col == 8 ? row + 1 : row;
+            col = col == 8 ? 0 : col + 1;
+        }
+        if (row == 9) {
+            return true;
+        }
+        // 1~9 试过去
+        for (int num = 1; num <= 9; num++) {
+            if (rowsUsed[row][num] || colsUsed[col][num] || cubesUsed[cubeNum(row, col)][num]) {
+                continue;
+            }
+            rowsUsed[row][num] = colsUsed[col][num] = cubesUsed[cubeNum(row, col)][num] = true;
+            board[row][col] = (char) (num + '0');
+            if (backtracking(row, col)) {
+                return true;
+            }
+            board[row][col] = '.';
+            rowsUsed[row][num] = colsUsed[col][num] = cubesUsed[cubeNum(row, col)][num] = false;
+        }
+        return false;
+    }
+
+    private static int cubeNum(int i, int j) {
+        int r = i / 3;
+        int c = j / 3;
+        return r * 3 + c;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{{'.', '.', '9', '7', '4', '8', '.', '.', '.'}, {'7', '.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '2', '.', '1', '.', '9', '.', '.', '.'}, {'.', '.', '7', '.', '.', '.', '2', '4', '.'}, {'.', '6', '4', '.', '1', '.', '5', '9', '.'}, {'.', '9', '8', '.', '.', '.', '3', '.', '.'}, {'.', '.', '.', '8', '.', '3', '.', '2', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.', '6'}, {'.', '.', '.', '2', '7', '5', '9', '.', '.'}};
+        solveSudoku(board);
+        System.out.println(Arrays.deepToString(board));
+    }
+}
---
+++ 算法思想/搜索/51. N皇后/src/Solution.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    /** 最好先创建一个 char[][]，并在此放置皇后，当 n == row 时，直接加入 char[]
+     * 否则全部操作都在 n == row 时进行，会很慢 */
+    private static char[][] nQueens;
+    private static boolean[] columnUsed;
+    private static boolean[] topLeftUsed;
+    private static boolean[] topRightUsed;
+    private static int n;
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> solution = new ArrayList<>();
+        columnUsed = new boolean[n];
+        topLeftUsed = new boolean[2 * n - 1];
+        topRightUsed = new boolean[2 * n - 1];
+        Solution.n = n;
+        nQueens = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(nQueens[i], '.');
+        }
+        backtrace(solution, 0);
+        return solution;
+    }
+
+    private static void backtrace(List<List<String>> solution, int row) {
+        if (n == row) {
+            List<String> list = new ArrayList<>();
+            for (char[] chars : nQueens) {
+                list.add(String.valueOf(chars));
+            }
+            solution.add(list);
+            return;
+        }
+        for (int column = 0; column < n; column++) {
+            int topLeftIndex = column + n - 1 - row;
+            int topRightIndex = column + row;
+            if (columnUsed[column] || topLeftUsed[topLeftIndex] || topRightUsed[topRightIndex]) {
+                continue;
+            }
+            nQueens[row][column] = 'Q';
+            columnUsed[column] = topLeftUsed[topLeftIndex] = topRightUsed[topRightIndex] = true;
+            backtrace(solution, row + 1);
+            columnUsed[column] = topLeftUsed[topLeftIndex] = topRightUsed[topRightIndex] = false;
+            nQueens[row][column] = '.';
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solveNQueens(4));
+    }
+}
