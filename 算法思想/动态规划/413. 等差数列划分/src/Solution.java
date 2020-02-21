4a7c6a4aec903c4e11d1f1dbdcf728dd05a607ab
JingqingLin <JingqingLin@live.com>
2020-02-21 15:23:15 +0800

    update .gitignore

---
+++ 算法思想/动态规划/413. 等差数列划分/src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    /** 两个数不满足等差 */
+    public static int numberOfArithmeticSlices(int[] A) {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
