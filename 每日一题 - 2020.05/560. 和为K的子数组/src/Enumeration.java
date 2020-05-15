bb27aaf761e2c1a12a4ff09cbb8b88ba2c65f150
JingqingLin <JingqingLin@live.com>
2020-05-15 17:41:07 +0800

    每日一题

---
+++ 每日一题 - 2020.05/560. 和为K的子数组/src/Enumeration.java
@@ -0,0 +1,19 @@
+public class Enumeration {
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; ++start) {
+            int sum = 0;
+            for (int end = start; end >= 0; --end) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 每日一题 - 2020.05/560. 和为K的子数组/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Solution {
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subarraySum(new int[]{1}, 0));
+    }
+}
