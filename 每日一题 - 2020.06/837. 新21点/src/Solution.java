54db24f844acf28a9d1cc4d7da46b23bdc93930f
JingqingLin <JingqingLin@live.com>
2020-06-03 10:34:51 +0800

    Create Solution.java

---
+++ 每日一题 - 2020.06/837. 新21点/src/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static double new21Game(int N, int K, int W) {
+        if (K == 0) {
+            return 1.0;
+        }
+        double[] dp = new double[K + W];
+        for (int i = K; i <= N && i <= K + W - 1; i++) {
+            dp[i] = 1.0;
+        }
+        dp[K - 1] = 1.0 * Math.min(N - K + 1, W) / W;
+        for (int i = K - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W;
+        }
+
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new21Game(10, 1, 10));
+    }
+}
